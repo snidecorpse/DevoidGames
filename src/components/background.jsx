c7a41800b2e7871f964f03b4843cfdc40b6df47e
rahilthehill <69742190+rahilthehill@users.noreply.github.com>
2025-02-02 04:40:21 -0500

    clean up mount react error fix again

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -245,17 +245,20 @@ const Background = () => {
     // CLEANUP ON UNMOUNT
     // ==================================================
     return () => {
-      window.removeEventListener("resize", onWindowResize);
-      window.removeEventListener("mousemove", onMouseMove);
-      gui.destroy();
-      if (galaxyPoints) {
-        galaxyGeometry.dispose();
-        galaxyMaterial.dispose();
-        scene.remove(galaxyPoints);
-      }
-      mountRef.current.removeChild(renderer.domElement);
-      renderer.dispose();
-    };
+        window.removeEventListener("resize", onWindowResize);
+        window.removeEventListener("mousemove", onMouseMove);
+        gui.destroy();
+        if (galaxyPoints) {
+          galaxyGeometry.dispose();
+          galaxyMaterial.dispose();
+          scene.remove(galaxyPoints);
+        }
+        // Check if mountRef.current exists before trying to remove the renderer
+        if (mountRef.current) {
+          mountRef.current.removeChild(renderer.domElement);
+        }
+        renderer.dispose();
+      };
   }, []);
 
   return <div ref={mountRef} className="webgl"></div>;
