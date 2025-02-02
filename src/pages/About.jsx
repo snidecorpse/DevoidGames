178a585030a281cb2c5528d9efbaeb9223245d65
TaufiqulK <khantauf@msu.edu>
2025-02-02 03:43:12 -0500

    about us final FINAL update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,8 +8,8 @@ const About = () => {
       <p>Hi! We are Aries Studios, a team of passionate developers who love making cool, interactive websites and video games. Enjoy your trip through our computer-generated stars!</p>
       
       <div className="social-links">
-        <a href="https://https://github.com/snidecorpse/DevoidGames.com" target="_blank" rel="noopener noreferrer" className="social-button github"> </a>
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="social-button x"> </a>
+        <a href="https://https://github.com/snidecorpse/DevoidGames" target="_blank" rel="noopener noreferrer" className="social-button github"> </a>
+        <a href="https://x.com" target="_blank" rel="noopener noreferrer" className="social-button x"> </a>
         <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="social-button instagram"> </a>
       </div>
     </div>
