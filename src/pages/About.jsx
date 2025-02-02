edd46da5be900c067698d656fdbd39f8857fa342
TaufiqulK <khantauf@msu.edu>
2025-02-02 03:33:57 -0500

    about us updt

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="about-container">
       <h1>About Us</h1>
-      <p>Welcome to our amazing app! We strive to create the best experience for our users.</p>
+      <p>Hi! We are Aries Studios, a team of passionate developers who love making cool, interactive websites and video games Enjoy your trip to the stars!</p>
       
       <div className="social-links">
         <a href="https://https://github.com/snidecorpse/DevoidGames.com" target="_blank" rel="noopener noreferrer" className="social-button github"> </a>
